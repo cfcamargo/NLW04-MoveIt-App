dca65599fa2a2a55178fa837f8f2a3d5889a893c
Cristian Camargo <chris.camargo2015@gmail.com>
2022-10-06 17:44:41 -0400

    first commit: configure project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-screen flex  justify-center items-center bg-background">
+    <h1 class="text-red text-3xl">
+      Hello World
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
